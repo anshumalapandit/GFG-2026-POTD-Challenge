270a445347bfcb7c9bf1e897a548ef3cb4afc689
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2026-01-06 16:10:19 +0530

    Update Max Xor Subarray of size K.java

--- January-Month-POTD/Max Xor Subarray of size K.java
+++ January-Month-POTD/Max Xor Subarray of size K.java
@@ -13,3 +13,32 @@ class Solution {
         return max;
     }
 }
+// optimal - sliding window 
+class Solution {
+    public int maxSubarrayXOR(int[] arr, int k) {
+        // code here
+        int max=0;
+        int cur=0;
+        int i=0,j=0;
+        int n=arr.length;
+        while(j<n){
+            // operation
+            cur=cur^arr[j];
+            // first achieved window size
+            if(j-i+1!=k){
+                j++;
+                continue;
+            }else{
+                // ans nikalo
+                max=Math.max(max,cur);
+                // remove ith calculation from cur
+                // i want to remove ith element ka calculation doing xor with curXor will make that part 0.
+                cur^=arr[i];
+                // now expand the window.
+                i++;
+                j++;
+            }
+        }
+        return max;
+    }
+}
