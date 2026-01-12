683b137d7fcc7f66b1a1113a8e82812fba1313ca
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2026-01-12 16:37:22 +0530

    Update K Sized Subarray Maximum.java

--- January-Month-POTD/K Sized Subarray Maximum.java
+++ January-Month-POTD/K Sized Subarray Maximum.java
@@ -54,3 +54,84 @@ class Solution {
         return ans;
     }
 }
+// optimal + list data structure
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+        int i=0;
+        int j=0;
+        while(j<arr.length){
+            // j k pehle jitne bhi lesser element = never used =popped
+            while(list.size()>0 && list.get(list.size()-1)<arr[j]){
+                list.remove(list.size()-1);
+            }
+            // jese hi remove ho jayega add current jth ele
+            list.add(arr[j]);
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                // if window size acheieved hai then try to maintain it
+                // 1. ans calculate kro calculation mai se
+                ans.add(list.get(0));
+                // 2. remove calculation b4 sliding the window .
+                // slide kese karte i->i++, j->j++
+                // ith wala element ko remove karna padega
+                // if ith wala present hoga to pehle hi present hoga bcoz we inserted only kaam ka value .
+                if(arr[i]==list.get(0)){
+                    list.remove(0);
+                }
+                // slide the window
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+}
+// optimal + deque data strucrure
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        // code here
+        // ArrayList<Integer> list=new ArrayList<>();
+        Deque<Integer> dq=new LinkedList<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+        int i=0;
+        int j=0;
+        while(j<arr.length){
+            // j k pehle jitne bhi lesser element = never used =popped
+            // dq=> i will store only idx , only maximum elements k idx
+            while(dq.size()>0 && arr[dq.peekLast()]<arr[j]){
+            //We use peekLast() in the while loop because:
+
+// 👉 We want to remove smaller elements that came before the current element
+// 👉 Those smaller elements are at the BACK of the deque
+// 👉 So we compare with last, not first
+
+                dq.pollLast();
+            }
+            // jese hi remove ho jayega add current jth ele
+            dq.add(j);
+            
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                // if window size acheieved hai then try to maintain it
+                // 1. ans calculate kro calculation mai se
+                ans.add(arr[dq.peekFirst()]); // jo pehle hoga wahi mera ans
+                // 2. remove calculation b4 sliding the window .
+                // slide kese karte i->i++, j->j++
+                // ith wala element ko remove karna padega
+                // if ith wala present hoga to pehle hi present hoga bcoz we inserted only kaam ka value .
+                if(dq.peekFirst()==i){
+                    dq.pollFirst();
+                }
+                // slide the window
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+}
